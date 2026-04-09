cde1624d10914e6230432e03eab59ee1bef7d7f8
CodeOS99 <kush.kuhu@gmail.com>
2026-04-09 15:13:06 +0530

    Added particles to the player while walking

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,18 +8,20 @@ func _process(delta: float) -> void:
 	$Pivot.rotation = lerp_angle($Pivot.rotation, angle+PI/2, 0.5)
 
 func _physics_process(delta: float) -> void:
-	var direction_arr := [
+	var direction_vec := Vector2(
 		Input.get_axis("left", "right"),
 		Input.get_axis("up", "down")
-		]
-	if direction_arr[0]:
-		velocity.x = direction_arr[0] * SPEED
+		)
+	if direction_vec.x:
+		velocity.x = direction_vec.x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED/16)
 		
-	if direction_arr[1]:
-		velocity.y = direction_arr[1] * SPEED
+	if direction_vec.y:
+		velocity.y = direction_vec.y * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED/16)
+
+	$Pivot/WalkParticles.visible = direction_vec.length() != 0
 
 	move_and_slide()
