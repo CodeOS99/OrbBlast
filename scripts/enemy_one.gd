b20d5502cdf2e75a996b94e47cab0f1b68b881c7
CodeOS99 <kush.kuhu@gmail.com>
2026-04-09 22:14:26 +0530

    Added a death explosion for the enemy

--- scripts/enemy_one.gd
+++ scripts/enemy_one.gd
@@ -8,6 +8,8 @@ var target_pos = Vector2.ZERO
 
 var speed = CHASE_SPEED
 
+var particles = preload("res://scenes/explosion.tscn")
+
 func _process(delta: float) -> void:
 	var angle = get_angle_to(Globals.player.global_position)
 	$Pivot.rotation = lerp_angle($Pivot.rotation, angle + PI/2, 0.1)
@@ -19,7 +21,12 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if (target_pos != Vector2.ZERO and self.global_position.distance_to(target_pos) < 1):
+		var instance = particles.instantiate()
+		instance.emitting = true
+		get_tree().root.add_child(instance)
+		instance.global_position = self.global_position
 		self.queue_free()
+	
 	var dir = global_position.direction_to(Globals.player.global_position if target_pos == Vector2.ZERO else target_pos)
 	velocity = dir * speed
 	
