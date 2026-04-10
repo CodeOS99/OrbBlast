3cbd2d70b835e75d0fc0cc15d68f52f63c8247e8
CodeOS99 <kush.kuhu@gmail.com>
2026-04-10 17:25:42 +0530

    Made the player spike attack work on all enemies

--- scripts/enemy_one.gd
+++ scripts/enemy_one.gd
@@ -10,6 +10,8 @@ var speed = CHASE_SPEED
 
 var particles = preload("res://scenes/explosion.tscn")
 
+@export var health = 1
+
 func _process(delta: float) -> void:
 	var angle = get_angle_to(Globals.player.global_position)
 	$Pivot.rotation = lerp_angle($Pivot.rotation, angle + PI/2, 0.1)
@@ -21,13 +23,26 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if (target_pos != Vector2.ZERO and self.global_position.distance_to(target_pos) < 1):
-		var instance = particles.instantiate()
-		instance.emitting = true
-		get_tree().root.add_child(instance)
-		instance.global_position = self.global_position
-		self.queue_free()
+		destroy()
 	
 	var dir = global_position.direction_to(Globals.player.global_position if target_pos == Vector2.ZERO else target_pos)
 	velocity = dir * speed
 	
 	move_and_slide()
+
+func damage():
+	health -= 1
+	if health <= 0:
+		destroy()
+	
+	var instance = particles.instantiate()
+	instance.emitting = true
+	get_tree().root.add_child(instance)
+	instance.global_position = self.global_position
+
+func destroy():
+	var instance = particles.instantiate()
+	instance.emitting = true
+	get_tree().root.add_child(instance)
+	instance.global_position = self.global_position
+	self.queue_free()
